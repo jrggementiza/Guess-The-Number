c6fe439572a8b4c58a552617bddb614bdaa2d0ff
jrggementiza <jrggementiza@gmail.com>
2018-06-20 13:49:35 +0800

    Refactored. Updated variable naming convention. Added Docstrings. Abstracted play_again() into a separate .py into generic.run_again() for possible future use.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,100 +1,90 @@
-""" User is promoted to guess a number between a minimum range and a maximum range 
+""" Guess the number! """
 
-To Add:
-Limited number of guesses
-"""
-import random
-
-def play_again():
-	""" Basic Error Handling """
-	while True:
-		try:
-			answer = input('would you like to play a again? [y/n] : ')
-			answer = answer.lower()
-			if answer == 'yes': answer = 'y'
-			if answer == 'no': answer = 'n'
-
-			assert answer == 'y' or answer =='n'
-
-		except AssertionError:
-			print('[y/n] only please!')
-
-		else:
-			return False if answer == 'n' else True
+import random, generic
 
 
-def check_answer(playerGuess, numberToGuess):
-	""" Working Answer """
-	returnValue = True
-	prompt = ''
+def check_answer(player_guess, guess_value):
+	"""
+	Compares a player's guess and the number to guess
+	Returns True if the player guessed correctly
+	Returns False by default
+	"""
 
-	if playerGuess > numberToGuess:
-		prompt = 'guess too high!'
-	elif playerGuess < numberToGuess:
-		prompt = 'guess too low!'
+	end_game = False
+
+	if player_guess > guess_value:
+		print('guess too high!')
+	elif player_guess < guess_value:
+		print('guess too low!')
 	else:
-		prompt = 'correct!'
-		returnValue = False
-	return prompt, returnValue
-
-	""" Trying to one line it; SyntaxError
-	returnValue = True
-	prompt = ''
-	prompt = 'guess too high!' if playerGuess > numberToGuess
-	prompt = 'guess too low!' if playerGuess < numberToGuess
-	prompt = 'guess correct!' and returnValue = False if playerGuess == numberToGuess
-	return propmt, returnValue
-	"""
-
-	""" Trying to use dictionary; KeyError
-	results = {
-	playerGuess > numberToGuess : ['guess too high!', True],
-	playerGuess < numberToGuess : ['guess too low!', True],
-	playerGuess == numberToGuess : ['guess correct!', False]
-	}
-
-	return results[playerGuess, numberToGuess]
-	"""
+		print('correct!')
+		end_game = True
+	return end_game
 
 
-def check_input(minGuess, maxGuess):
-	#add error handling: not int, outside range
+def check_input(min_guess_range, max_guess_range):
+	""" Asks user to enter guess and returns a guess within defined min and max guess range """
 	while True:
 		try:
 			playerGuess = int(input('enter your guess: '))
-			assert minGuess <= playerGuess <= maxGuess
+			assert min_guess_range <= playerGuess <= max_guess_range
 
 		except AssertionError:
-			print('guess should be between {0} - {1}!'.format(minGuess, maxGuess))
+			print('guess should be between {0} - {1}!'.format(min_guess_range, max_guess_range))
 		except ValueError:
 			print('numbers only!')
 		else:
 			return playerGuess
 
 
-def guess_number():
-	minGuess, maxGuess = 1, 10
-	numberToGuess = random.randrange(minGuess,maxGuess)
-	print('guess the number between {0} and {1}!'.format(minGuess, maxGuess))
-
-	noWinner = True
-	while noWinner == True:
-		playerGuess = check_input(minGuess, maxGuess)
-		prompt, noWinner = check_answer(playerGuess, numberToGuess)
-		print(prompt)
+def guess_number(min_guess_range, max_guess_range):
+	""" Returns a guess that is within defined min and max guess range """
+	print(f'guess the number between {min_guess_range} and {max_guess_range}!')
+	return check_input(min_guess_range, max_guess_range)
 
 
-def play():
-	runGame = True
-	while runGame == True:
-		guess_number()
-		runGame = play_again()
-	print('GOOD BYE!')
+def generate_guess_value(min_guess_range=1, max_guess_range=10):
+	"""
+	Returns a random number to guess between a defined min and max range.
+	Min and Max range can be custom. Default values are 1 - 10
+	"""
+	return random.randrange(min_guess_range, max_guess_range), min_guess_range, max_guess_range
 
 
 def main():
-	play()
+	run_game = True
+	while run_game == True:
+		guess_value, min_guess_range, max_guess_range = generate_guess_value(1,4)
+		guess_count, guess_limit = 1, 3
 
+		end_game = False
+		while end_game == False:
+			print(f'You have {guess_limit - guess_count + 1} remaining. ')
+			player_guess = guess_number(min_guess_range, max_guess_range)
+			player_won = check_answer(player_guess, guess_value)
+			guess_count = guess_count + 1
+			
+			if player_won == True:
+				print(f'You win! congrats! ')
+				end_game = True
+			elif guess_count > guess_limit:
+				print(f'You ran out of guesses! you lose!')
+				end_game = True
+
+		run_game = generic.run_again()
 
 if __name__ == '__main__':
 	main()
+
+# [*] number to guess is generated within min - max range
+# [*] guess limit is set
+# [*] guess is made -> check guess within range
+# [*] guess made compared to number to guess
+# [*] loop again until guess limit runs out or until guess made matchess number to guess
+
+
+
+
+
+
+
